e500c3435c11daea839d3772c3187f27d3266f74
Ian McFarlane <i.mcfarlane2002@gmail.com>
2025-01-05 22:40:53 -0600

    added range_traits public header before removal

--- include/okay/iterable/range_traits.h
+++ include/okay/iterable/range_traits.h
@@ -1,30 +1,11 @@
 #ifndef __OKAYLIB_ITERABLE_RANGE_TRAITS_H__
 #define __OKAYLIB_ITERABLE_RANGE_TRAITS_H__
 
-#include "okay/detail/range_traits.h"
+#include "okay/iterable/range_accessors.h"
 
 #ifndef OKAYLIB_NOEXCEPT
 #define OKAYLIB_NOEXCEPT noexcept
 #endif
-
-namespace ok {
-/// Find the beginning of an iterable, using its default cursor type.
-inline constexpr auto const& begin = detail::begin_fn_defaulted_t{};
-
-/// Find the beginning of an iterable for a specific cursor type.
-template <typename cursor_t>
-inline constexpr auto const& begin_for_cursor = detail::begin_fn_t<cursor_t>{};
-
-/// Find the sentinel of an iterable, using its default cursor/sentinel type.
-/// Return type: sentinel associated with iterable and cursor. (default
-/// sentinel)
-inline constexpr auto const& end = detail::end_fn_defaulted_t{};
-
-/// Find the sentinel of an iterable for a specific cursor type.
-/// Return type: sentinel associated with iterable and cursor.
-template <typename cursor_t>
-inline constexpr auto const& end_for_cursor = detail::end_fn_t<cursor_t>{};
-} // namespace ok
 
 namespace ok {
 namespace detail {
