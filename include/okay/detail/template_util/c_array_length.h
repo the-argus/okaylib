8f0165e3a857366f4d7a547f034ef8ea5b9d9f63
Ian McFarlane <i.mcfarlane2002@gmail.com>
2025-03-29 12:27:38 -0400

    added c_array_length_t which deduces c array length without needing an instance of the type

--- include/okay/detail/template_util/c_array_length.h
+++ include/okay/detail/template_util/c_array_length.h
@@ -2,6 +2,7 @@
 #define __OKAYLIB_DETAIL_TEMPLATE_UTIL_C_ARRAY_LENGTH_H__
 
 #include <cstddef>
+#include <type_traits>
 
 namespace ok::detail {
 
@@ -10,6 +11,12 @@ inline constexpr size_t c_array_length(T (&)[sz])
 {
     return sz;
 }
+
+template <typename T, size_t sz>
+std::integral_constant<size_t, sz> c_array_length_deduction(T (&)[sz]);
+
+template <typename T>
+using c_array_length_t = decltype(c_array_length_deduction(std::declval<T>()));
 
 } // namespace ok::detail
 
