fcb68d7d2ebd6802b82094689fcd3ed43eeaa958
Ian McFarlane <i.mcfarlane2002@gmail.com>
2025-11-09 21:14:03 -0500

    allow addressof to be constexpr with STD and NO_STD compat strategies

--- include/okay/detail/addressof.h
+++ include/okay/detail/addressof.h
@@ -1,11 +1,23 @@
 #ifndef __OKAYLIB_DETAIL_OK_ADDRESSOF_H__
 #define __OKAYLIB_DETAIL_OK_ADDRESSOF_H__
 
+#include "okay/detail/type_traits.h"
+#if defined(OKAYLIB_COMPAT_STRATEGY_STD)
+#include <utility>
+#endif
+
 namespace ok {
 template <class T> constexpr T* addressof(T& arg) noexcept
 {
+#if defined(OKAYLIB_COMPAT_STRATEGY_STD)
+    return ::std::addressof(arg);
+#elif defined(OKAYLIB_COMPAT_STRATEGY_NO_STD)
+    return __builtin_addressof(arg);
+#elif defined(OKAYLIB_COMPAT_STRATEGY_PURE_CPP)
+    // NOTE: cannot be constexpr
     return reinterpret_cast<T*>(
         &const_cast<char&>(reinterpret_cast<char const volatile&>(arg)));
+#endif
 }
 } // namespace ok
 
