9a31f116cd06eef9c3729b11d2139e1ce636864b
Ian McFarlane <i.mcfarlane2002@gmail.com>
2025-11-11 11:32:08 -0500

    make use of new res .transform_value() function for freeing a slice

--- tests/reserving_page_allocator/reserving_page_allocator.cpp
+++ tests/reserving_page_allocator/reserving_page_allocator.cpp
@@ -1,3 +1,4 @@
+#include "okay/defer.h"
 #include "test_header.h"
 // test header must be first
 #include "okay/allocators/reserving_page_allocator.h"
@@ -38,27 +39,39 @@ TEST_SUITE("reserving page allocator")
         {
             reserving_page_allocator_t ally({.pages_reserved = 2});
 
+            const auto free_res = [&](const alloc::result_t<bytes_t>& res) {
+                ally.deallocate(
+                    res.transform_value(
+                           &bytes_t::unchecked_address_of_first_item)
+                        .copy_or(nullptr));
+            };
+
             size_t page_size = mmap::get_page_size();
 
             auto res = ally.allocate(alloc::request_t{
                 .num_bytes = page_size,
             });
+            defer f1 = [&] { free_res(res); };
 
             auto res2 = ally.allocate(alloc::request_t{
                 .num_bytes = page_size - 1,
             });
+            defer f2 = [&] { free_res(res2); };
 
             auto res3 = ally.allocate(alloc::request_t{
                 .num_bytes = page_size + 1,
             });
+            defer f3 = [&] { free_res(res3); };
 
             // cannot allocate over reserved page numbers
             auto res4 = ally.allocate(alloc::request_t{
                 .num_bytes = page_size * 2,
             });
+            defer f4 = [&] { free_res(res4); };
             auto res5 = ally.allocate(alloc::request_t{
                 .num_bytes = (page_size * 2) + 1,
             });
+            defer f5 = [&] { free_res(res5); };
 
             REQUIRE(res.is_success());
             REQUIRE(res2.is_success());
